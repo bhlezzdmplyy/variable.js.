2c6bdb536809a099540e122aff675fb2b9d2716c
bhlezzdmplyy <felicitybhlezzgarbin@gmail.com>
2024-09-09 20:17:13 +0800

    Create add.js

---
+++ add.js
@@ -0,0 +1,11 @@
+// Declare and Intialize the variables
+let name = "felicity bhlezz";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "female";
+let twitterHandle = "@bhlezzmchi";
+
+//Log the variables to the console
+console.log("Name " + name);
+console.log("What do you want to become: " + whatDoYouWannaBecomeInYourLife);
+console.log("Gender: " + gender);
+console.log("Twitter Handle: " + twitterHandle);
